5d6ba9bfd404c832fec88a2e7589cddc547f6b2e
Noah <noah.terminello@gmail.com>
2022-10-27 23:04:16 -0700

    working on explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,21 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+
 function init() {
-  // TODO
+  loadVoices();
+
 }
+
+function loadVoices(){
+  let voiceSelect = document.querySelector("#voice-select");
+  let voices = speechSynthesis.getVoices();
+  console.log(voices);
+  let voiceOptions = [];
+  for(let i = 0; i < voices.length; i++){
+    let voiceOption = new Option();
+    voiceOption.text = voices[i].name;
+    console.log(voiceOption.text);
+  }
+}
+
